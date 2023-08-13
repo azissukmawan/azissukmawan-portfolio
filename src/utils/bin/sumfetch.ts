6572bd712ddadb5ae1a8fcd09acbfa1d1394aca6
amawanzs <142155781+amawanzs@users.noreply.github.com>
2023-08-13 20:58:33 +0700

    Update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -16,6 +16,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
      CONTACT
      <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
      <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+     <u><a href="https://www.linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
     -----------
 `;
 };
